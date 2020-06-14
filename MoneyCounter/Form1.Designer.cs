dc6360fd48f7395197c8bf5080457508b92c5c05
therealponga1 <you@example.comlukegilchrist69@hotmail.com>
2020-06-14 23:59:20 +1200

    Add project files.

---
+++ MoneyCounter/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace MoneyCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCount = new System.Windows.Forms.Button();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.res100 = new System.Windows.Forms.TextBox();
+            this.res50 = new System.Windows.Forms.TextBox();
+            this.res20 = new System.Windows.Forms.TextBox();
+            this.res10 = new System.Windows.Forms.TextBox();
+            this.res5 = new System.Windows.Forms.TextBox();
+            this.res2 = new System.Windows.Forms.TextBox();
+            this.res1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(319, 143);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(97, 39);
+            this.btnCount.TabIndex = 0;
+            this.btnCount.Text = "Go!";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(272, 97);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(196, 20);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(175, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(109, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "$100";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(184, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "$50";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(251, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "$20";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(321, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "$10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(391, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "$5";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(464, 250);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "$2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(532, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "$1";
+            // 
+            // res100
+            // 
+            this.res100.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.res100.Location = new System.Drawing.Point(112, 266);
+            this.res100.Name = "res100";
+            this.res100.ReadOnly = true;
+            this.res100.Size = new System.Drawing.Size(65, 20);
+            this.res100.TabIndex = 10;
+            // 
+            // res50
+            // 
+            this.res50.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.res50.Location = new System.Drawing.Point(183, 266);
+            this.res50.Name = "res50";
+            this.res50.ReadOnly = true;
+            this.res50.Size = new System.Drawing.Size(65, 20);
+            this.res50.TabIndex = 11;
+            // 
+            // res20
+            // 
+            this.res20.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.res20.Location = new System.Drawing.Point(254, 266);
+            this.res20.Name = "res20";
+            this.res20.ReadOnly = true;
+            this.res20.Size = new System.Drawing.Size(64, 20);
+            this.res20.TabIndex = 12;
+            // 
+            // res10
+            // 
+            this.res10.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.res10.Location = new System.Drawing.Point(324, 266);
+            this.res10.Name = "res10";
+            this.res10.ReadOnly = true;
+            this.res10.Size = new System.Drawing.Size(64, 20);
+            this.res10.TabIndex = 13;
+            // 
+            // res5
+            // 
+            this.res5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.res5.Location = new System.Drawing.Point(394, 266);
+            this.res5.Name = "res5";
+            this.res5.ReadOnly = true;
+            this.res5.Size = new System.Drawing.Size(64, 20);
+            this.res5.TabIndex = 14;
+            // 
+            // res2
+            // 
+            this.res2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.res2.Location = new System.Drawing.Point(464, 266);
+            this.res2.Name = "res2";
+            this.res2.ReadOnly = true;
+            this.res2.Size = new System.Drawing.Size(65, 20);
+            this.res2.TabIndex = 15;
+            // 
+            // res1
+            // 
+            this.res1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.res1.Location = new System.Drawing.Point(535, 266);
+            this.res1.Name = "res1";
+            this.res1.ReadOnly = true;
+            this.res1.Size = new System.Drawing.Size(65, 20);
+            this.res1.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(0, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(281, 201);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(172, 24);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Number Of Notes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 435);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.res1);
+            this.Controls.Add(this.res2);
+            this.Controls.Add(this.res5);
+            this.Controls.Add(this.res10);
+            this.Controls.Add(this.res20);
+            this.Controls.Add(this.res50);
+            this.Controls.Add(this.res100);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.btnCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox res100;
+        private System.Windows.Forms.TextBox res50;
+        private System.Windows.Forms.TextBox res20;
+        private System.Windows.Forms.TextBox res10;
+        private System.Windows.Forms.TextBox res5;
+        private System.Windows.Forms.TextBox res2;
+        private System.Windows.Forms.TextBox res1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ MoneyCounter/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoneyCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            int[] Coinvals = new int[] { 1, 2, 5, 10, 20, 50, 100 };
+            int[] NumofCoinsUsed = new int[7];
+            // If txtAmount is not numerical then notify user and exit
+            int Amount2Cal = Convert.ToInt32(txtAmount.Text);
+            int RemainingAmount = Amount2Cal;
+
+            for (int currentCoin = 6; 
+                    currentCoin >= 0; currentCoin--) 
+            {
+                int currentCoinIndex = RemainingAmount / Coinvals[currentCoin];
+                
+                NumofCoinsUsed[currentCoin] = currentCoinIndex;
+                RemainingAmount -= (currentCoinIndex * Coinvals[currentCoin]);
+
+                if (RemainingAmount == 0) break;
+            }
+
+            res100.Text = NumofCoinsUsed[6].ToString();
+            res50.Text = NumofCoinsUsed[5].ToString();
+            res20.Text = NumofCoinsUsed[4].ToString();
+            res10.Text = NumofCoinsUsed[3].ToString();
+            res5.Text = NumofCoinsUsed[2].ToString();
+            res2.Text = NumofCoinsUsed[1].ToString();
+            res1.Text = NumofCoinsUsed[0].ToString();
+
+
+        }
+    }
+}
